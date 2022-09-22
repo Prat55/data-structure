8316cd2353abe55552e2ba809e962e1836db7e7f
Prat55 <92040737+Prat55@users.noreply.github.com>
2022-09-22 10:28:27 +0530

    Add files via upload

--- pract2.py
+++ pract2.py
@@ -1,56 +1,133 @@
+import gc
+
 class Node:
- def __init__(self,data):
-     self.data = data;
-     self.previous = None;
-     self.next = None;
+
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+        self.prev = None
+
 
 class DoublyLinkedList:
- 
- def __init__(self):
-     self.head = None;
-     self.tail = None;
 
- 
- def addNode(self, data):
- 
-     newNode = Node(data);
+    def __init__(self):
+        self.head = None
 
- 
-     if(self.head == None):
- 
-         self.head = self.tail = newNode;
-         self.head.previous = None;
-         self.tail.next = None;
-     else:
- 
-         self.tail.next = newNode;
- 
- newNode.previous = self.tail;
- 
- self.tail = newNode;
- 
- self.tail.next = None;
+    
+    def insert_front(self, data):
 
- 
- def display(self):
- 
-     current = self.head;
-     if(self.head == None):
-         print("List is empty");
-         return;
-    print("Nodes of doubly linked list: ");
-     while(current != None):
- 
-         print(current.data),;
-     current = current.next;
+        
+        new_node = Node(data)
 
-dList = DoublyLinkedList();
+        
+        new_node.next = self.head
 
-dList.addNode(1);
-dList.addNode(2);
-dList.addNode(3);
-dList.addNode(4);
-dList.addNode(5);
+       
+        if self.head is not None:
+            self.head.prev = new_node
+
+        
+        self.head = new_node
+
+    
+    def insert_after(self, prev_node, data):
+
+        
+        if prev_node is None:
+            print("previous node cannot be null")
+            return
+
+        
+        new_node = Node(data)
+
+        
+        new_node.next = prev_node.next
+
+        
+        prev_node.next = new_node
+
+        
+        new_node.prev = prev_node
+
+        
+        if new_node.next:
+            new_node.next.prev = new_node
+
+    
+    def insert_end(self, data):
+
+        
+        new_node = Node(data)
+
+        
+        if self.head is None:
+            self.head = new_node
+            return
+
+        
+        temp = self.head
+
+        
+        while temp.next:
+            temp = temp.next
+
+        
+        temp.next = new_node
+
+        
+        new_node.prev = temp
+
+        return
+
+    
+    def deleteNode(self, dele):
+
+        
+        if self.head is None or dele is None:
+            return
+
+        
+        if self.head == dele:
+            self.head = dele.next
+
+        
+        if dele.next is not None:
+            dele.next.prev = dele.prev
+
+        
+        if dele.prev is not None:
+            dele.prev.next = dele.next
+
+        
+        gc.collect()
+
+    
+    def display_list(self, node):
+
+        while node:
+            print(node.data, end="->")
+            last = node
+            node = node.next
 
 
-dList.display();
+
+d_linked_list = DoublyLinkedList()
+
+d_linked_list.insert_end(5)
+d_linked_list.insert_front(1)
+d_linked_list.insert_front(6)
+d_linked_list.insert_end(9)
+
+
+d_linked_list.insert_after(d_linked_list.head, 11)
+
+
+d_linked_list.insert_after(d_linked_list.head.next, 15)
+
+d_linked_list.display_list(d_linked_list.head)
+
+
+d_linked_list.deleteNode(d_linked_list.head.next.next.next.next.next)
+
+print()
+d_linked_list.display_list(d_linked_list.head)
